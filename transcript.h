9b9ab4ce703ecc6f1645f376b2b863ec2542a8c0
colesse <colesse>
2000-05-01 23:00:08 +0000

    added function prototypes.

--- transcript.h
+++ transcript.h
@@ -1,30 +1,44 @@
 #include <sys/stat.h>
-#include <sys/param.h>
 
 #define T_EOF      0  
-#define NOPRINT    0
-#define PRINT	   1 
-#define POS	   1
-#define NEG	   0
-#define SPECIAL    2
-#define NOMOVE	   0
-#define MOVE	   1
+
+#define T_POSITIVE   1
+#define T_NEGATIVE   -1
+#define T_SPECIAL    0
+
+#define T_MOVE_BOTH  0
+#define T_MOVE_FS    -1
+#define T_MOVE_TRAN  1
+
+#define T_MODE	     0x0FFF
+
+#define FS2TRAN	0
+#define TRAN2FS 1
+
+int	edit_path;
+FILE    *outtran;
 
 struct info {
-    char		    name[ MAXPATHLEN ];
-    struct stat		    stat;
-    char		    link[ MAXPATHLEN ];
-    int			    chksum;
-    major_t		    maj;
-    minor_t		    min;
+    char		    i_name[ MAXPATHLEN ];
+    char		    i_link[ MAXPATHLEN ];
+    struct stat		    i_stat;
+    int			    i_chksum;
+    major_t		    i_maj;
+    minor_t		    i_min;
+    char		    i_type;
 };
 
 struct transcript {
     struct info		    t_info;
     struct transcript       *t_next;
     int		    	    t_type;
-    char		    file_name[ MAXPATHLEN ];
-    mode_t		    type;
+    int			    t_form;
+    char		    t_name[ MAXPATHLEN ];
+    mode_t		    t_mode;
     int			    t_flag;
     FILE		    *t_in;
 };
+ 
+int transcript( struct info *, char *, FILE * );
+void transcript_init( void );
+void transcript_free( void );
