9ed4c6959403d4a7d48ba54bdd6747b6c8c04104
wes <wes>
2000-08-25 17:20:47 +0000

    hardlink_free() removed unused fields in transcript fixed white space change constants to positive ints

--- transcript.h
+++ transcript.h
@@ -5,47 +5,42 @@
 #define T_NEGATIVE	2 
 #define T_SPECIAL	3
 
-#define T_MOVE_TRAN  1
-#define T_MOVE_FS    -1
-#define T_MOVE_BOTH  0
+#define T_MOVE_TRAN	1
+#define T_MOVE_FS	2
+#define T_MOVE_BOTH	3 
 
-#define T_MODE	     0x0FFF
+#define T_MODE		0x0FFF
 
-#define FS2TRAN	0
-#define TRAN2FS 1
+#define FS2TRAN		0
+#define TRAN2FS		1
 
-#define FLAG_SKIP    ( 1<<0 )
-#define FLAG_INIT    ( 1<<1 )
+#define FLAG_SKIP	( 1 << 0 )
+#define FLAG_INIT	( 1 << 1 )
 
-int	edit_path;
-FILE    *outtran;
+int			edit_path;
+FILE			*outtran;
 
 struct info {
-    char		    i_name[ MAXPATHLEN ];
-    char		    i_link[ MAXPATHLEN ];
-    struct stat		    i_stat;
-    int			    i_chksum;
-#ifdef notdef
-    major_t		    i_maj;
-    minor_t		    i_min;
-#endif
-    dev_t		    i_dev;
-    char		    i_type;
+    char		i_type;
+    char		i_name[ MAXPATHLEN ];
+    char		i_link[ MAXPATHLEN ];
+    struct stat		i_stat;
+    int			i_chksum;
+    dev_t		i_dev;
 };
 
 struct transcript {
-    struct info		    t_info;
-    struct transcript       *t_next;
-    int		    	    t_type;
-    int			    t_form;
-    char		    t_name[ MAXPATHLEN ];
-    mode_t		    t_mode;
-    int			    t_eof;
-    FILE		    *t_in;
+    struct transcript	*t_next;
+    struct info		t_info;
+    int 		t_type;
+    char		t_name[ MAXPATHLEN ];
+    int			t_linenum;
+    int			t_eof;
+    FILE		*t_in;
 };
  
 int	transcript( struct info *, char * );
 void	transcript_init( int );
 void	transcript_free( void );
 char	*hardlink( struct info *info );
-void	d_free( void );
+void	hardlink_free( void );
