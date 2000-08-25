25d913cbc5bb58a3cc54ebf7c300fdfcc721bb75
wes <wes>
2000-08-25 17:23:13 +0000

    exit on errors to reduce user confusion fixed white space made usage better

--- fsdiff.c
+++ fsdiff.c
@@ -16,7 +16,7 @@
 #include "llist.h"
 #include "code.h"
 
-char    *version = VERSION;
+char	*version = VERSION;
 
 int	main( int, char ** );
 void	fs_walk( struct llist *, int );
@@ -25,11 +25,11 @@ void	fs_walk( struct llist *, int );
 fs_walk( struct llist *path, int flag ) 
 {
     DIR			*dir;
-    struct dirent 	*de;
+    struct dirent	*de;
     struct llist	*head = NULL;
     struct llist	*new;
     struct llist	*cur;
-    char 		temp[ MAXPATHLEN ];
+    char		temp[ MAXPATHLEN ];
 
     /* call the transcript code */
     if (( transcript( &path->ll_info, path->ll_info.i_name ) == 0 ) ||
@@ -40,7 +40,7 @@ fs_walk( struct llist *path, int flag )
     /* open directory */
     if (( dir = opendir( path->ll_info.i_name )) == NULL ) {
 	perror( path->ll_info.i_name );
-	return;
+	exit( 1 );	
     }
 
     /* read contents of directory */
@@ -64,17 +64,17 @@ fs_walk( struct llist *path, int flag )
 	    sprintf( temp, "%s", de->d_name );
 	}
 
-        /* allocate new node for newly created relative pathname */
-        new = ll_allocate( temp );
+	/* allocate new node for newly created relative pathname */
+	new = ll_allocate( temp );
 
-   	/* insert new file into the list */
+	/* insert new file into the list */
 	ll_insert( &head, new ); 
 
     }
 
     if ( closedir( dir ) != 0 ) {
-    	perror( "closedir" );
-	return;
+	perror( "closedir" );
+	exit( 1 );
     }
 
     /* call fswalk on each element in the sorted list */
@@ -91,13 +91,13 @@ fs_walk( struct llist *path, int flag )
 main( int argc, char **argv ) 
 {
     struct llist	*root;
-    extern char    	*optarg;
-    extern int    	optind;
+    extern char 	*optarg;
+    extern int		optind;
 #ifndef linux
     extern int		errno;
 #endif
-    int    		c;
-    int    		errflag = 0;
+    int 		c;
+    int 		errflag = 0;
     int			flag = 0;	/* XXX do we need a flag  */  
 
     flag |= FLAG_INIT;
@@ -117,7 +117,7 @@ main( int argc, char **argv )
 	    flag |= FLAG_SKIP;
 	    break;	
 
-	case 't': 		/* want to record differences from tran */
+	case 't':		/* want to record differences from tran */
 	    edit_path = FS2TRAN;
 	    break;
 
@@ -134,18 +134,12 @@ main( int argc, char **argv )
 	}
     }
 
-    if ( errflag ) {
-	fprintf( stderr, "usage: fsdiff [ -t | -1 | -V | -o <file> ]\n" );
+    if (( edit_path == FS2TRAN ) && ( flag & FLAG_SKIP )) {
+	errflag++;
     }
 
-    if ( argv[ optind ] == NULL ) {
-	fprintf( stderr, "ERROR: A valid path name is required\n" );
-	exit( 1 );
-    }
-
-    if ( argc - optind > 1 ) {
-	fprintf( stderr,
-	    "ERROR: One and only one valid path name is allowed!\n" );
+    if ( errflag || ( argc - optind != 1 )) {
+	fprintf( stderr, "usage: fsdiff [ -t | -1 ] [ -o file ] path\n" );
 	exit ( 1 );
     }
 
@@ -153,21 +147,21 @@ main( int argc, char **argv )
     transcript_init( flag );
 
     if ( flag & FLAG_SKIP ) {
-    	root = ll_allocate( argv[ optind ] );
+	root = ll_allocate( argv[ optind ] );
     } else if ( chdir( argv[ optind ] ) != 0 ) {
 	if ( errno != ENOTDIR ) {
 	    perror( argv[ optind ] );
 	    exit( 1 );
 	}
-    	root = ll_allocate( argv[ optind ] );
+	root = ll_allocate( argv[ optind ] );
     } else {
-    	root = ll_allocate( "." );
+	root = ll_allocate( "." );
     }
     fs_walk( root, flag );
 
     /* free the transcripts */
     transcript_free( );
-    d_free( );
+    hardlink_free( );
 	    
     /* close the output file */     
     fclose( outtran );
