2765839bf20a06771814591997f1aad638c2fe50
editor <editor>
2001-05-04 21:01:19 +0000

    Broke code up

--- lapply.c
+++ lapply.c
@@ -19,39 +19,15 @@
 #include "argcargv.h"
 #include "code.h"
 #include "base64.h"
+#include "convert.h"
+#include "download.h"
 
 #define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
 
 void		(*logger)( char * ) = NULL;
 struct timeval 	timeout = { 10 * 60, 0 };
 int		linenum = 0;
-
-    static char
-t_convert( int type )
-{
-    switch( type ) {
-    case S_IFREG:
-	return ( 'f' );
-    case S_IFDIR:
-	return ( 'd' );
-    case S_IFLNK:
-	return ( 'l' );
-    case S_IFCHR:
-	return ( 'c' );
-    case S_IFBLK:
-	return ( 'b' );
-#ifdef SOLARIS
-    case S_IFDOOR:
-	return ( 'D' );
-#endif
-    case S_IFIFO:
-	return ( 'p' );
-    case S_IFSOCK:
-	return ( 's' );
-    default:
-	return ( 0 );
-    }
-}
+int		verbose = 0;
 
     static int
 ischild( const char *parent, const char *child)
@@ -73,127 +49,6 @@ ischild( const char *parent, const char *child)
 	}
     }
 }
-    int
-download( SNET *sn, char *transcript, char *path, char *chksum ) 
-{
-    struct timeval      tv;
-    char 		*line;
-    char                *temppath;
-    unsigned int	rr;
-    int			fd;
-    size_t              size;
-    char                buf[ 8192 ]; 
-    unsigned char	md[ SHA_DIGEST_LENGTH ];
-    unsigned char	mde[ SZ_BASE64_E( sizeof( md ) ) ];
-    SHA_CTX		sha_ctx;
-
-    /* Connect to server */ 
-    if( snet_writef( sn, "RETR FILE %s %s\r\n", transcript,
-	    encode( path ) ) == NULL ) {
-	perror( "snet_writef" );
-	return( -1 );
-    }
-
-    printf( ">>> RETR FILE %s %s\r\n", transcript, encode( path ) );
-
-    tv = timeout;
-    if ( ( line = snet_getline_multi( sn, logger, &tv ) ) == NULL ) {
-	perror( "snet_getline_multi" );
-	return( -1 );
-    }
-
-    printf( "<<< %s\n", line );
-
-    if ( *line != '2' ) {
-	perror( line );
-	return( -1 );
-    }
-
-    /*Create temp file name*/
-    temppath = (char *)malloc( MAXPATHLEN );
-    if ( temppath == NULL ) {
-	perror( "malloc" );
-	return ( -1 );
-    }
-    sprintf( temppath, "%s.radmind.%i", path, rand() );
-
-    /* Open file */
-    if ( ( fd = open( temppath, O_WRONLY | O_CREAT | O_EXCL, 0 ) ) < 0 ) {
-	perror( temppath );
-	return( -1 );
-    }
-    tv = timeout;
-    if ( ( line = snet_getline( sn, &tv ) ) == NULL ) {
-	perror( "snet_getline 1" );
-	goto error;
-    }
-
-    size = atoi( line );
-    printf( "<<< %i\n", size );
-    printf( "<<< " );
-
-    SHA1_Init( &sha_ctx );
-
-    /* Get file from server */
-    while ( size > 0 ) {
-	tv = timeout;
-	if ( ( rr = snet_read( sn, buf, MIN( sizeof( buf ), size ),
-		&tv ) ) <= 0 ) {
-	    perror( "snet_read" );
-	    goto error;
-	}
-	SHA1_Update( &sha_ctx, buf, rr );
-	if ( write( fd, buf, (int)rr ) != rr ) {
-	    perror( temppath );
-	    goto error;
-	}
-	printf( "..." );
-	size -= rr;
-    }
-
-    printf( "\n" );
-
-    if ( close( fd ) != 0 ) {
-	perror( temppath );
-	goto error;
-    }
-    SHA1_Final( md, &sha_ctx );
-    base64_e( md, sizeof( md ), mde );
-    if ( ( strcmp( chksum, mde ) ) != 0  && strcmp( chksum, "-" ) !=0 ) {
-	perror( "Chksum failed" );
-	goto error;
-    }
-    if ( rename( temppath, path ) != 0 ) {
-	perror( path );
-	goto error;
-    }
-
-    free( temppath );
-
-    if ( size != 0 ) {
-	perror( "Did not write correct number of bytes" );
-	return( -1 );
-    }
-
-    tv = timeout;
-    if ( ( line = snet_getline( sn, &tv ) ) == NULL ) {
-	perror( "snet_getline" );
-	return( -1 );
-    }
-    if ( strcmp( line, "." ) != 0 ) {
-	perror( line );
-	return( -1 );
-    }
-
-    return ( 0 );
-
-error:
-    close( fd );
-    unlink( temppath );
-    free ( temppath );
-    return ( -1 );
-
-}
 
     int 
 apply( FILE *f, char *parent, SNET *sn )
@@ -209,11 +64,10 @@ apply( FILE *f, char *parent, SNET *sn )
     struct utimbuf	times;
     uid_t		uid;
     gid_t		gid;
+    dev_t		dev;
 
     while ( fgets( tline, MAXPATHLEN, f ) != NULL ) {
 	linenum++;
-
-	//printf( "%d: %s", linenum, tline );
 
 	len = strlen( tline );
         if (( tline[ len - 1 ] ) != '\n' ) {
@@ -224,7 +78,7 @@ apply( FILE *f, char *parent, SNET *sn )
 	tac = argcargv( tline, &targv );
 
 	if ( tac == 1 ) {
-	    printf( "Command file: %s\n", targv[ 0 ] );
+	    if ( verbose ) printf( "Command file: %s\n", targv[ 0 ] );
 	    strcpy( transcript, targv[ 0 ] );
 	} else {
 
@@ -245,20 +99,17 @@ apply( FILE *f, char *parent, SNET *sn )
 		perror( path );
 		return( 1 );
 	    } else {
-		//printf( "%s not present\n", path );
 		present = 0;
 	    }
 
-	    if ( present && ( *command == '-'||  fstype != *targv[ 0 ] ) ) {
-
-		printf( "%s: Must Remove\n", path );
+	    if ( present && ( *command == '-' ||  fstype != *targv[ 0 ]
+		    || fstype == 'l' ) ) {
 
 		if ( fstype == 'd' ) {
-		    printf( "Calling apply to handle dir\n" );
 
 		    /* Recurse */
 		    if ( apply( f, path, sn ) != 0 ) {
-			return -1;
+			return( 1 );
 		    }
 
 		    /* Directory is now empty */
@@ -266,39 +117,15 @@ apply( FILE *f, char *parent, SNET *sn )
 			perror( path );
 			return( 1 );	
 		    }
-
 		    present = 0;
-
 		} else {
-		    printf( "Unlinking %s for update\n", path );
 		    if ( unlink( path ) != 0 ) {
 			perror( path );
 			return( 1 );
 		    }
-
 		    present = 0;
 		}
-	    }
-
-	    if ( !present && *command != '+' ) {
-		switch ( *targv[ 0 ] ) {
-		case 'd':
-		    mode = strtol( targv[ 2 ], (char**)NULL, 8 );
-		    if ( mkdir( path, mode ) != 0 ) {
-			perror( path );
-			return( 1 );
-		    }
-		    if ( lstat( path, &st ) != 0 ) {
-			perror( path );
-			return( 1 );
-		    }
-		    present = 1;
-		    printf( "    %s created\n", path );
-		    break;
-		default:
-		    perror( "Unkown create type" );
-		    return( 1 );
-		}
+		if ( verbose ) printf( "%s deleted\n", path );
 	    }
 
 	    /* DOWNLOAD */
@@ -319,67 +146,78 @@ apply( FILE *f, char *parent, SNET *sn )
 		present = 1;
 	    }
 
-	    if ( present ) {
+	    if ( *command != '-' ) {
 
 		/* UPDATE */
-
 		switch ( *targv[ 0 ] ) {
 		case 'f':
-
 		    if ( tac != 8 ) {
 			// IS THIS OKAY FOR ERROR
 			printf( "%s: incorrect number of arguments\n", tline );
 			return( 1 );
 		    }
-
 		    mode = strtol( targv[ 2 ], (char **)NULL, 8 );
 		    uid = atoi( targv[ 3 ] );
 		    gid = atoi( targv[ 4 ] );
 		    times.modtime = atoi( targv[ 5 ] );
-
 		    if( mode != st.st_mode ) {
 			if ( chmod( path, mode ) != 0 ) {
 			    perror( path );
 			    return( 1 );
 			}
-			printf( "    Updating %s mode\n", path );
+			if ( verbose ) printf( "    Updating %s mode\n", path );
 		    }
-
 		    if( uid != st.st_uid  || gid != st.st_gid ) {
 			if ( lchown( path, uid, gid ) != 0 ) {
 			    perror( path );
 			    return( 1 );
 			}
 			if ( uid != st.st_uid ) {
-			    printf( "    Updating %s uid\n", path );
+			    if ( verbose ) printf( "    Updating %s uid\n", path );
 			}
 			if ( gid != st.st_gid ) {
-			    printf( "    Updating %s gid\n", path );
+			    if ( verbose ) printf( "    Updating %s gid\n", path );
 			}
 		    }
 
 		    if( times.modtime != st.st_mtime ) {
 			
 			/*Is this what I should to for access time? */
+
 			times.actime = st.st_atime;
 			if ( utime( path, &times ) != 0 ) {
 			    perror( path );
 			    return( 1 );
 			}
-			printf( "    Updating %s time\n", path ); 
+			if ( verbose ) printf( "    Updating %s time\n", path ); 
 		    }
 		    break;
 
 		case 'd':
 
 		    if ( tac != 5 ) {
-			fprintf( stderr, "%s: incorrect number of arguments\n", tline );
+			fprintf( stderr,
+			    "%s: incorrect number of arguments\n", tline );
 			return( 1 );
 		    }
 
 		    mode = strtol( targv[ 2 ], (char **)NULL, 8 );
 		    uid = atoi( targv[ 3 ] );
 		    gid = atoi( targv[ 4 ] );
+
+		    if ( !present ) {
+			mode = strtol( targv[ 2 ], (char**)NULL, 8 );
+			if ( mkdir( path, mode ) != 0 ) {
+			    perror( path );
+			    return( 1 );
+			}
+			if ( lstat( path, &st ) != 0 ) {
+			    perror( path );
+			    return( 1 );
+			}
+			present = 1;
+			if ( verbose ) printf( "    %s created\n", path );
+		    }
 
 		    /* check mode */
 		    if( mode != st.st_mode ) {
@@ -387,6 +225,7 @@ apply( FILE *f, char *parent, SNET *sn )
 			    perror( path );
 			    return( 1 );
 			}
+			if ( verbose ) printf( "    Updating %s mode\n", path );
 		    }
 
 		    /* check uid & gid */
@@ -395,36 +234,47 @@ apply( FILE *f, char *parent, SNET *sn )
 			    perror( path );
 			    return( 1 );
 			}
+			if ( uid != st.st_uid ) {
+			    if ( verbose ) printf( "    Updating %s uid\n", path );
+			}
+			if ( gid != st.st_gid ) {
+			    if ( verbose ) printf( "    Updating %s gid\n", path );
+			}
 		    }
 		    break;
 
 		case 'h':
-
 		    if ( tac != 3 ) {
-			fprintf( stderr, "%s: incorrect number of arguments\n", tline );
+			fprintf( stderr,
+			    "%s: incorrect number of arguments\n", tline );
 			return( 1 );
 		    }
-		    if ( link( decode( targv[ 2 ] ), path ) != 0 ) {
+		    if ( link( targv[ 2 ], path ) != 0 ) {
 			perror( path );
 			return( 1 );
 		    }
+		    if ( verbose ) printf( "    %s hard link to %s created\n",
+			path, targv[ 2 ] );
 		    break;
 
 		case 'l':
-
 		    if ( tac != 3 ) {
-			fprintf( stderr, "%s: incorrect number of arguments\n", tline );
+			fprintf( stderr,
+			    "%s: incorrect number of arguments\n", tline );
 			return( 1 );
 		    }
-		    if ( symlink( decode( targv[ 2 ] ), path ) != 0 ) {
+		    if ( symlink( targv[ 2 ] , path ) != 0 ) {
 			perror( path );
 			return( 1 );
 		    }
+		    if ( verbose ) printf( "    %s symbolic link to %s created\n",
+			path, targv[ 2 ] );
 		    break;
 
 		case 'p':
 		    if ( tac != 5 ) { 
-			fprintf( stderr, "%s: incorrect number of arguments\n", tline );
+			fprintf( stderr,
+			    "%s: incorrect number of arguments\n", tline );
 			return( 1 );
 		    }
 		    mode = strtol( targv[ 2 ], (char **)NULL, 8 );
@@ -440,6 +290,7 @@ apply( FILE *f, char *parent, SNET *sn )
 			    perror( path );
 			    return( 1 );
 			}
+			if ( verbose ) printf( "    Updating %s mode\n", path );
 		    }
 		    /* check uid & gid */
 		    if( uid != st.st_uid  || gid != st.st_gid ) {
@@ -447,14 +298,31 @@ apply( FILE *f, char *parent, SNET *sn )
 			    perror( path );
 			    return( 1 );
 			}
+			if ( uid != st.st_uid ) {
+			    if ( verbose ) printf( "    Updating %s uid\n", path );
+			}
+			if ( gid != st.st_gid ) {
+			    if ( verbose ) printf( "    Updating %s gid\n", path );
+			}
 		    }
 		    break;
 
 		case 'b':
 		case 'c':
 		    if ( tac != 7 ) {
-			fprintf( stderr, "%s: incorrect number of arguments\n", tline );
+			fprintf( stderr,
+			    "%s: incorrect number of arguments\n", tline );
 			return( 1 );
+		    }
+
+		    if ( present && ( ( minor( st.st_rdev )
+			    != atoi( targv[ 6 ] ) ) || ( major( st.st_rdev )
+			    != atoi( targv[ 5 ] ) ) ) ) {
+			if ( unlink( path ) != 0 ) {
+			    perror( path );
+			    return( 1 );
+			}
+			present = 0;
 		    }
 
 		    mode = strtol( targv[ 2 ], (char **)NULL, 8 );
@@ -466,7 +334,12 @@ apply( FILE *f, char *parent, SNET *sn )
 		    uid = atoi( targv[ 3 ] );
 		    gid = atoi( targv[ 4 ] );
 		    if ( !present ) {
-			if ( mknod( path, mode, makedev( atoi( targv[ 6 ] ), atoi( targv[ 7 ] ) ) != 0 ) ) {
+			if ( ( dev = makedev( atoi( targv[ 5 ] ),
+				atoi( targv[ 6 ] ) ) ) == NODEV ) {
+			    perror( path );
+			    return( 1 );
+			}
+			if ( mknod( path, mode, dev ) != 0 ) {
 			    perror( path );
 			    return( 1 );
 			}
@@ -475,6 +348,7 @@ apply( FILE *f, char *parent, SNET *sn )
 			    return( 1 );
 			}
 			present = 1;
+			if ( verbose ) printf( "%s created\n", path );
 		    }
 		    /* check mode */
 		    if( mode != st.st_mode ) {
@@ -482,12 +356,19 @@ apply( FILE *f, char *parent, SNET *sn )
 			    perror( path );
 			    return( 1 );
 			}
+			if ( verbose ) printf( "Updating: %s mode\n", path );
 		    }
 		    /* check uid & gid */
 		    if( uid != st.st_uid  || gid != st.st_gid ) {
 			if ( lchown( path, uid, gid ) != 0 ) {
 			    perror( path );
 			    return( 1 );
+			}
+			if ( uid != st.st_uid ) {
+			    if ( verbose ) printf( "Updating %s uid\n", path );
+			}
+			if ( gid != st.st_gid ) {
+			    if ( verbose ) printf( "Updating %s gid\n", path );
 			}
 		    }
 		    break;
@@ -522,7 +403,7 @@ main( int argc, char **argv )
     SNET		*sn;
     struct timeval	tv;
 
-    while ( ( c = getopt ( argc, argv, "h:np:V" ) ) != EOF ) {
+    while ( ( c = getopt ( argc, argv, "h:np:Vv" ) ) != EOF ) {
 	switch( c ) {
 	case 'h':
 	    host = optarg;
@@ -540,8 +421,11 @@ main( int argc, char **argv )
 	    }
 	    break;
 	case 'V':
-	     printf( "%s\n", version );
-	     exit( 0 );
+	    printf( "%s\n", version );
+	    exit( 0 );
+	case 'v':
+	    verbose = 1;
+	    break;
 	case '?':
 	    err++;
 	    break;
@@ -552,7 +436,7 @@ main( int argc, char **argv )
     }
 
     if ( err || ( argc - optind != 1 ) ) {
-	fprintf( stderr, "usage: lapply [ -n ] " );
+	fprintf( stderr, "usage: lapply [ -nv ] " );
 	fprintf( stderr, "[ -h host ] [ -port ] " );
 	fprintf( stderr, "difference-transcript\n" );
 	exit( 1 );
@@ -574,15 +458,15 @@ main( int argc, char **argv )
 	    sin.sin_port = port;
 	    memcpy( &sin.sin_addr.s_addr, he->h_addr_list[ i ],
 		( unsigned int)he->h_length );
-	    printf( "trying %s... ",
+	    if ( verbose ) printf( "trying %s... ",
 		    inet_ntoa( *( struct in_addr *)he->h_addr_list[ i ] ) );
 	    if ( connect( s, ( struct sockaddr *)&sin,
 		    sizeof( struct sockaddr_in ) ) != 0 ) {
-		printf( "connect" );
+		perror( "connect" );
 		(void)close( s );
 		continue;
 	    }
-	    printf( "success!\n" );
+	    if ( verbose ) printf( "success!\n" );
 
 	    if ( ( sn = snet_attach( s, 1024 * 1024 ) ) == NULL ) {
 		perror ( "snet_attach failed" );
