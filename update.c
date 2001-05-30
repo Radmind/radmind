19c24346b51a5e827dfcefc820e8629526b3eeaa
editor <editor>
2001-05-30 16:43:27 +0000

    cleared out include files

--- update.c
+++ update.c
@@ -1,31 +1,15 @@
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <netdb.h>
-#include <sha.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/socket.h>
 #include <sys/mkdev.h>
-#include <sys/ddi.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-#include <strings.h>
 #include <utime.h>
-#include <errno.h>
 
 #include "snet.h"
-#include "argcargv.h"
-#include "code.h"
-#include "base64.h"
-#include "convert.h"
-#include "download.h"
 #include "update.h"
 
 extern int verbose;
 extern int linenum;
-
 
     int
 update( char *path, int present, struct stat st, int tac, char **targv )
