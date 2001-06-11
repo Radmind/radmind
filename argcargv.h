559c5a0cc5072c8108ceb2e5a43adfb8294fc290
editor <editor>
2001-06-11 19:46:25 +0000

    fixed reentrant issue

--- argcargv.h
+++ argcargv.h
@@ -1,1 +1,9 @@
-int argcargv( char *, char *** );
+#define argcargv(X, Y) (acav_parse( NULL, X, Y ))
+
+typedef struct {
+    unsigned acv_argc;
+    char **acv_argv;
+} ACAV;
+ACAV* acav_alloc( void );
+int acav_parse( ACAV *acav, char *, char *** );
+int acav_free( ACAV *acav );
