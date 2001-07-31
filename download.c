7a80316b9d68d0ac7760ef36efbf505ebe4212f7
editor <editor>
2001-07-31 20:55:31 +0000

    added chksum.h updated to new version of download - get works as old download did - download returns path to temp file using new versiong of chksum.h seeded random number

--- download.c
+++ download.c
@@ -5,10 +5,12 @@
 #include <sys/ddi.h>
 #include <unistd.h>
 
+
 #include "snet.h"
 #include "code.h"
 #include "base64.h"
 #include "download.h"
+#include "chksum.h"
 
 #define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
 
@@ -19,7 +21,31 @@ extern int		verbose;
 extern int		chksum;
 
     int
-download( SNET *sn, char *transcript, char *path, char *chksumval ) 
+get( SNET *sn, char *pathdesc, char *path, char *chksumval )
+{
+    char 	*temppath;
+
+    if ( ( temppath = download( sn, pathdesc, path, chksumval ) ) == NULL ) {
+	fprintf( stderr, "%s: unable to download\n", path );
+	return( 1 );
+    }
+    if ( rename( temppath, path ) != 0 ) {
+	perror( temppath );
+	return( 1 );
+    }
+    free( temppath );
+
+    return( 0 );
+}
+
+/*
+ * Download requests path from sn and writes it to disk.  The path to
+ * this new file is returned upon success and must be freed by the caller.
+ * NULL is returned otherwise.
+ */
+
+    char *
+download( SNET *sn, char *pathdesc, char* path, char *chksumval ) 
 {
     struct timeval      tv;
     char 		*line;
@@ -28,120 +54,116 @@ download( SNET *sn, char *transcript, char *path, char *chksumval )
     int			fd;
     size_t              size;
     char                buf[ 8192 ]; 
-    unsigned char	md[ SHA_DIGEST_LENGTH ];
-    unsigned char	mde[ SZ_BASE64_E( sizeof( md ) ) ];
-    SHA_CTX		sha_ctx;
+    unsigned char	chksumcalc[ 29 ];
 
     if ( chksum && ( strcmp( chksumval, "-" ) == 0 ) ) {
-	perror( "Chksum not in transcript" );
-	return( -1 );
+	fprintf( stderr, "Chksum not in transcript" );
+	return( NULL );
     }
 
     /* Connect to server */ 
-    if( snet_writef( sn, "RETR FILE %s %s\n", transcript,
-	    encode( path ) ) == NULL ) {
-	perror( "snet_writef" );
-	return( -1 );
+    if( snet_writef( sn, "RETR %s\n", pathdesc ) == NULL ) {
+	fprintf( stderr, "snet_writef" );
+	return( NULL );
     }
 
-    if ( verbose ) printf( ">>> RETR FILE %s %s\n", transcript,
-	encode( path ) );
+    if ( verbose ) printf( ">>> RETR %s\n", pathdesc );
 
     tv = timeout;
     if ( ( line = snet_getline_multi( sn, logger, &tv ) ) == NULL ) {
-	perror( "snet_getline_multi" );
-	return( -1 );
+	fprintf( stderr, "snet_getline_multi" );
+	return( NULL );
     }
 
-    if ( verbose ) printf( "<<< %s\n", line );
-
     if ( *line != '2' ) {
-	perror( line );
-	return( -1 );
+	fprintf( stderr, "%s", line );
+	return( NULL );
     }
 
     /*Create temp file name*/
     temppath = (char *)malloc( MAXPATHLEN );
-    if ( temppath == NULL ) {
-	perror( "malloc" );
-	return ( -1 );
+    if ( temppath == NULL ) { perror( "malloc" );
+	return ( NULL );
     }
+    srand( (unsigned int)getpid() );
     sprintf( temppath, "%s.radmind.%i", path, rand() );
 
     /* Open file */
-    if ( ( fd = open( temppath, O_WRONLY | O_CREAT | O_EXCL, 0 ) ) < 0 ) {
+    if ( ( fd = open( temppath, O_RDWR | O_CREAT | O_EXCL, 0666 ) ) < 0 ) {
 	perror( temppath );
-	return( -1 );
+	goto error3;
     }
     tv = timeout;
     if ( ( line = snet_getline( sn, &tv ) ) == NULL ) {
-	perror( "snet_getline 1" );
+	fprintf( stderr, "snet_getline" );
 	goto error;
     }
-
     size = atoi( line );
-    if ( verbose ) printf( "<<< %i\n", size );
-    if ( verbose ) printf( "<<< " );
 
-    if ( chksum ) SHA1_Init( &sha_ctx );
+    if ( verbose ) printf( "<<< " );
 
     /* Get file from server */
     while ( size > 0 ) {
 	tv = timeout;
 	if ( ( rr = snet_read( sn, buf, (int)MIN( sizeof( buf ), size ),
 		&tv ) ) <= 0 ) {
-	    perror( "snet_read" );
+	    fprintf( stderr, "snet_read" );
 	    goto error;
 	}
-	if ( chksum ) SHA1_Update( &sha_ctx, buf, rr );
 	if ( write( fd, buf, (size_t)rr ) != rr ) {
 	    perror( temppath );
 	    goto error;
 	}
-	if ( verbose ) printf( "..." );
+	if ( verbose ) printf( "." );
 	size -= rr;
     }
 
-    if ( close( fd ) != 0 ) {
-	perror( temppath );
+    if ( verbose ) printf( "\n" );
+
+    if ( size != 0 ) {
+	fprintf( stderr, "Did not write correct number of bytes" );
+	goto error;
+    }
+    tv = timeout;
+    if ( ( line = snet_getline( sn, &tv ) ) == NULL ) {
+	fprintf( stderr, "snet_getline" );
+	goto error;
+    }
+    if ( strcmp( line, "." ) != 0 ) {
+	fprintf( stderr, "%s", line );
 	goto error;
     }
     if ( chksum ) {
-	SHA1_Final( md, &sha_ctx );
-	base64_e( md, sizeof( md ), mde );
-	if ( strcmp( chksumval, mde ) != 0 ) {
-	    perror( "Chksum failed" );
+	if ( verbose ) printf( "*** chksum " );
+
+	if ( lseek( fd, 0, SEEK_SET ) != 0 ) {
+	    perror( "lseek" );
 	    goto error;
 	}
+
+	if( do_chksum_fd( fd, chksumcalc ) != 0 ) {
+	    fprintf( stderr, "do_chksum_fd failed" );
+	    goto error;
+	}
+
+	if ( strcmp( chksumval, chksumcalc ) != 0 ) {
+	    fprintf( stderr, " mismatch" );
+	    goto error;
+	}
+	if ( verbose ) printf( "match\n" );
     }
-    if ( rename( temppath, path ) != 0 ) {
+    if ( close( fd ) != 0 ) {
 	perror( path );
-	goto error;
+	goto error2;
     }
-
-    free( temppath );
-
-    if ( size != 0 ) {
-	perror( "Did not write correct number of bytes" );
-	return( -1 );
-    }
-
-    tv = timeout;
-    if ( ( line = snet_getline( sn, &tv ) ) == NULL ) {
-	perror( "snet_getline" );
-	return( -1 );
-    }
-    if ( strcmp( line, "." ) != 0 ) {
-	perror( line );
-	return( -1 );
-    }
-
-    return ( 0 );
+    return ( temppath );
 
 error:
     close( fd );
+error2:
     unlink( temppath );
+error3:
     free ( temppath );
-    return ( -1 );
+    return ( NULL );
 
 }
