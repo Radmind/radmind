0ed6e3fccb9a5c1c34f25cb7a3f344b8e58f5383
editor <editor>
2001-08-27 18:24:44 +0000

    removoed download() combined into retr()

--- download.h
+++ download.h
@@ -1,3 +1,1 @@
-int retr( SNET *sn, char *pathdesc, char *path, char *file, char *chksumval );
-char * download( SNET *sn, char *pathdesc, char *path, char *file,
-	char *chksumval ); 
+char * retr( SNET *sn, char *pathdesc, char *path, char *chksumval ); 
