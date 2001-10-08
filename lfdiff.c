8e18061f663ebafe9587c918c72d2553b49bab70
editor <editor>
2001-10-08 19:45:16 +0000

    set system error exit code to -1 removed -d option added -X "diff options" updated usage removed -l diff option

--- lfdiff.c
+++ lfdiff.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <netdb.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <strings.h>
 
 #include "snet.h"
 #include "download.h"
@@ -32,32 +34,39 @@ output( char *string )
 /*
  * exit codes:
  *      0       No changes found, everything okay
- *      2       System error
+ *      -1       System error
  */
 
     int
 main( int argc, char **argv, char **envp )
 {
-    int			c, i, port = htons( 6662 ), err = 0, special = 0, tac = 0;
+    int			c, i, tac, port = htons( 6662 ), err = 0;
+    int			special = 0, diffargc = 0;
     int			fd;
-    extern int          optind;
+    extern int          optind, optopt;
     extern char		*version;
     char		*host = NULL;
     char		*temppath = NULL;
     char		*transcript = NULL;
-    char		*diffopts = NULL;
     char		*file = NULL;
     char		**diffargv;
-    char		**targv;
+    char		**argcargv;
     char 		pathdesc[ 2 * MAXPATHLEN ];
+    char		opt[ 3 ];
     struct servent	*se;
     SNET		*sn;
 
-    while ( ( c = getopt ( argc, argv, "d:h:p:sT:Vv" ) ) != EOF ) {
+    /* create argv to pass to diff */
+    if ( ( diffargv = (char **)malloc( 1  * sizeof( char * ) ) ) == NULL ) {
+	perror( "malloc" );
+	exit( -1 );
+    }
+    diffargc = 0;
+    diffargv[ diffargc++ ] = diff;
+
+    while ( ( c = getopt ( argc, argv, "h:p:ST:VvbitwcefnC:D:sX:" ) )
+	    != EOF ) {
 	switch( c ) {
-	case 'd':
-	    diffopts = optarg;
-	    break;
 	case 'h':
 	    host = optarg;
 	    break;
@@ -65,12 +74,12 @@ main( int argc, char **argv, char **envp )
 	    if ( ( port = htons ( atoi( optarg ) ) ) == 0 ) {
 		if ( ( se = getservbyname( optarg, "tcp" ) ) == NULL ) {
 		    fprintf( stderr, "%s: service unkown\n", optarg );
-		    exit( 2 );
+		    exit( -1 );
 		}
 		port = se->s_port;
 	    }
 	    break;
-	case 's':
+	case 'S':
 	    special = 1;
 	    break;
 	case 'T':
@@ -82,6 +91,47 @@ main( int argc, char **argv, char **envp )
 	case 'v':
 	    verbose = 1;
 	    logger = output;
+	    break;
+	/* diff options */
+	case 'b': case 'i': case 't': case 'w':
+	case 'c': case 'e': case 'f': case 'n':
+	case 's':
+	    if ( ( diffargv = (char **)realloc( diffargv, ( sizeof( *diffargv )
+		    + ( 2 * sizeof( char * ) ) ) ) ) == NULL ) {
+		perror( "malloc" );
+		exit( -1 );
+	    }
+	    sprintf( opt, "-%c", c );
+	    if ( ( diffargv[ diffargc++ ] = strdup( opt ) ) == NULL ) {
+		perror( "strdup" );
+		exit( -1 );
+	    };
+	    break;
+	case 'C': case 'D': 
+	    if ( ( diffargv = (char **)realloc( diffargv, ( sizeof( *diffargv )
+		    + ( 3 * sizeof( char * ) ) ) ) ) == NULL ) {
+		perror( "malloc" );
+		exit( -1 );
+	    }
+	    sprintf( opt, "-%c", c );
+	    if ( ( diffargv[ diffargc++ ] = strdup( opt ) ) == NULL ) {
+		perror( "strdup" );
+		exit( -1 );
+	    };
+	    diffargv[ diffargc++ ] = optarg;
+	    break;
+	case 'X':
+	    if ( ( tac = argcargv( opt, &argcargv ) ) < 0 ) {
+		err++;
+	    }
+	    if ( ( diffargv = (char **)realloc( diffargv, ( sizeof( *diffargv )
+		    + ( tac * sizeof( char * ) ) ) ) ) == NULL ) {
+		perror( "malloc" );
+		exit( -1 );
+	    }
+	    for ( i = 0; i < tac; i++ ) {
+		diffargv[ diffargc++ ] = argcargv[ i ];
+	    }
 	    break;
 	case '?':
 	    err++;
@@ -100,18 +150,18 @@ main( int argc, char **argv, char **envp )
 
     if ( err || ( argc - optind != 1 ) ) {
 	fprintf( stderr, "usage: %s ", argv[ 0 ] );
-	fprintf( stderr, "[ -d \"diff opts\" ] [ -T transcript | -s ] " );
-	fprintf( stderr, "[ -h host ] [ -p port ] [ -vV ] " );
+	fprintf( stderr, "[ -T transcript | -S ] " );
+	fprintf( stderr, "[ -h host ] [ -p port ] [ -vV ] [ diff options ] " );
+	fprintf( stderr, "[ -X \"unsupported diff options\" ] " );
 	fprintf( stderr, "file\n" );
-	exit( 2 );
+	exit( -1 );
     }
     file = argv[ optind ];
 
     if( ( sn = connectsn( host, port )  ) == NULL ) {
 	fprintf( stderr, "%s: %d connection failed.\n", host, port );
-	exit( 2 );
+	exit( -1 );
     }
-    if ( verbose ) printf( "\n" );
 
     /* create path description */
     if ( special ) {
@@ -128,52 +178,43 @@ main( int argc, char **argv, char **envp )
 	}
     }
 
-    if ( verbose ) printf( "*** Retrieving file: %s\n", file); 
     if ( ( temppath = retr( sn, pathdesc, file, location, NULL ) )
 	    == NULL ) {
 	fprintf( stderr, "%s: retr failed\n", file );
-	exit( 1 );
+	exit( -1 );
     }
-    if ( verbose ) printf( "*** %s written to %s\n", file, location );
 
-    if ( verbose ) printf( "\n" );
-    if ( ( closesn( sn ) ) !=0 ) {
+    if ( ( closesn( sn ) ) != 0 ) {
 	fprintf( stderr, "can not close sn\n" );
-	exit( 1 );
+	exit( -1 );
     }
 
     if ( ( fd = open( temppath, O_RDONLY ) ) < 0 ) {
 	perror( temppath );
+	exit( -1 );
     } 
     if ( unlink( temppath ) != 0 ) {
 	perror( temppath );
-	exit( 1 );
+	exit( -1 );
     }
     if ( dup2( fd, 0 ) < 0 ) {
 	perror( temppath );
-	exit( 1 );
+	exit( -1 );
     }
-    /* create argv to pass to diff */
-    if ( diffopts != NULL ) {
-	tac = acav_parse( NULL, diffopts, &targv );
-    }
-    if ( ( diffargv = malloc( ( tac + 5 ) * sizeof( char * ) ) ) == NULL ) {
+    if ( ( diffargv = (char **)realloc( diffargv, ( sizeof( *diffargv )
+	    + ( 4 * sizeof( char * ) ) ) ) ) == NULL ) {
 	perror( "malloc" );
-	exit( 1 );
+	exit( -1 );
     }
-    diffargv[ 0 ] = diff;
-    for ( i = 0; i < tac; i++ ) {
-	diffargv[ i + 1 ] = (char *)targv[ i ];
-    }
-    diffargv[ tac + 1 ] = "--";
-    diffargv[ tac + 2 ] = "-";
-    diffargv[ tac + 3 ] = file; 
-    diffargv[ tac + 4 ] = NULL;
+    diffargv[ diffargc++ ] = "--";
+    diffargv[ diffargc++ ] = "-";
+    diffargv[ diffargc++ ] = file; 
+    diffargv[ diffargc++ ] = NULL;
 
     execve( diff, diffargv, envp );
 
     perror( diff );
     printf( "DIE DIE DIE\n" );
-    exit( 1 );
+    exit( -1 );
 }
 
