6567ca85f56e4be2d01fc646c6d73b2ebfc23bac
editor <editor>
2001-10-11 21:15:09 +0000

    added copy( );

--- copy.c
+++ copy.c
@@ -41,3 +41,39 @@ copyover( char *spath, char *dpath )
     }
     return( 0 );
 }
+    
+    int
+copy( char *spath, char *dpath )
+{
+    char	buff[ 8 * 1024 ];
+    int		src, dest;
+    size_t	len;
+
+    if ( ( src = open( spath, O_RDONLY) ) < 0 ) {
+	perror( spath );
+	return( 1 );
+    }
+    if ( ( dest = open( dpath, O_WRONLY |  O_CREAT | O_EXCL ) ) < 0 ) {
+	perror( dpath );
+	return( 1 );
+    }
+    while ( ( len = read( src, buff, (size_t)sizeof( buff ) ) )  > 0 ) {
+	if ( write( dest, buff, len ) != len ) {
+	    perror( spath );
+	    return( 1 );
+	}
+    }
+    if ( len < 0 ) {
+	perror( spath );
+	return( 1 );
+    }
+    if ( close( src ) < 0 ) {
+	perror( spath );
+	return( 1 );
+    }
+    if ( close( dest ) < 0 ) {
+	perror( dpath );
+	return( 1 );
+    }
+    return( 0 );
+}
