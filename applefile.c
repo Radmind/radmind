3ceb27b74a801ad64d8ba8f1be7f02dbe6928f79
editor <editor>
2002-02-11 21:43:58 +0000

    Added #ifdef __APPLE__ around apple includes. Made as_ents array for entry info. Added error cases.

--- applefile.c
+++ applefile.c
@@ -4,13 +4,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/uio.h>
+#ifdef __APPLE__
 #include <sys/paths.h>
 #include <sys/attr.h>
+#endif __APPLE__
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include <string.h>
 #include <snet.h>
 
@@ -20,67 +22,49 @@
 #include "chksum.h"
 #include "base64.h"
 
-extern struct timeval	timeout;
 extern int		verbose;
 extern int              chksum;
-
 void            (*logger)( char * );
+extern struct timeval	timeout;
 
-char			null_buf[ 32 ] = { 0 };
+struct attrlist		alist = {
+    ATTR_BIT_MAP_COUNT,
+    0,
+    ATTR_CMN_FNDRINFO,
+    0,
+    0,
+    0,
+    0,
+};
 
 struct as_header	as_header = {
-    0x00051600,
-    0x00020000,
+    AS_MAGIC,
+    AS_VERSION,
     {
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     },
-    NUM_ENTRIES,
+    AS_NENTRIES,
 };
 
     int
 retr_applefile( SNET *sn, char *pathdesc, char *path, char *chksumval,
     char *temppath, int linenum )
 {
-    int			rc;
-    int			ofd, rsrcfd, rsize, as_cc;
-    int			dodots = 0;
-    size_t		size;
-    unsigned char	finfo[ 32 ];
-    char		as_buf[ 8192 ];
+    int			dfd, rfd, rc, dodots = 0;
+    size_t		size, rsize;
+    char		finfo[ 32 ];
+    char		buf[ 8192 ];
     char		rsrc_path[ MAXPATHLEN ];
-    const char		*rsrc_suffix = _PATH_RSRCFORKSPEC;
-    struct as_header	as_dest;
-    struct as_entry	ae_finfo;
-    struct as_entry	ae_rfork;
-    struct as_entry	ae_dfork;
-    struct timeval	tv = timeout;
     char		*line;
+    struct as_header	ah;
+    struct as_entry	ae_ents[ 3 ]; 
+    struct timeval	tv;
     unsigned char       md[ SHA_DIGEST_LENGTH ];
     unsigned char       mde[ SZ_BASE64_E( sizeof( md )) ];
     SHA_CTX             sha_ctx;
 
-    struct attrlist		alist = {
-	ATTR_BIT_MAP_COUNT,
-	0,
-	ATTR_CMN_FNDRINFO,
-	0,
-	0,
-	0,
-	0,
-    };
-
-    memset( &as_dest, '\0', ( int )sizeof( struct as_header ));
-    memset( &ae_finfo, '\0', ( int )sizeof( struct as_entry ));
-    memset( &ae_rfork, '\0', ( int )sizeof( struct as_entry ));
-    memset( &ae_dfork, '\0', ( int )sizeof( struct as_entry ));
-
     if ( chksum ) {
-	if ( strcmp( chksumval, "-" ) == 0 ) {
-	    fprintf( stderr, "line %d: Chksum not listed\n", linenum );
-	    return( -1 );
-	}
-
 	SHA1_Init( &sha_ctx );
     }
 
@@ -101,224 +85,186 @@ retr_applefile( SNET *sn, char *pathdesc, char *path, char *chksumval,
         return( -1 );
     }
 
-    /* Create temp file name */
-    if ( snprintf( temppath, MAXPATHLEN, "%s.radmind.%i",
-	    path, getpid() ) > MAXPATHLEN ) {
-	fprintf( stderr, "%s.radmind.%i: too long", path,
-		(int)getpid() );
-	exit( 1 );
-    }
-
     /* Get file size from server */
     tv = timeout;
     if (( line = snet_getline( sn, &tv )) == NULL ) {
         fprintf( stderr, "snet_getline" );
-        exit( 1 );
+        return( -1 );
     }
-    size = atoi( line );
+    size = atol( line );
     if ( verbose ) printf( "<<< %d\n<<< ", size );
-
-    /*
-     * If output is not a tty, don't bother with the dots.
-     */
-    if ( verbose && isatty( fileno( stdout ))) {
-        dodots = 1;
-    }
 
     tv = timeout;
     /* read header to determine if file is encoded in applesingle */
-    if (( as_cc = snet_read( sn, ( char * )&as_dest, AS_HEADERLEN, &tv ))
-		<= 0 ) {
+    if (( rc = snet_read( sn, ( char * )&ah, AS_HEADERLEN, &tv ))
+		!= AS_HEADERLEN ) {
 	perror( "snet_read" ); 
-	exit( 1 );
+	return( -1 );
     }
-    if ( chksum ) SHA1_Update( &sha_ctx, ( char * )&as_dest, (size_t)as_cc );
-    if ( dodots ) { putc( '.', stdout ); fflush( stdout ); }
-    size -= as_cc;
 
-    if ( as_dest.ah_magic != AS_MAGIC 
-	    || as_dest.ah_version != AS_VERSION 
-	    || as_dest.ah_num_entries != NUM_ENTRIES ) {
+    if ( memcmp( &as_header, &ah, sizeof( struct as_header )) != 0 ) {
 	fprintf( stderr, "%s is not a radmind AppleSingle file.\n", path );
-	exit( 1 );
+	return( -1 );
     }
+    if ( chksum ) SHA1_Update( &sha_ctx, ( char * )&ah, (size_t)rc );
+    if ( dodots ) { putc( '.', stdout ); fflush( stdout ); }
+    size -= rc;
 
-    /* read finder info header entry */
+    /* read header entries */
     tv = timeout;
-    if (( as_cc = snet_read( sn, ( char * )&ae_finfo,
-		sizeof( struct as_entry ), &tv )) <= 0 ) {
+    if (( rc = snet_read( sn, ( char * )&ae_ents,
+		( 3 * sizeof( struct as_entry )), &tv ))
+		!= ( 3 * sizeof( struct as_entry ))) {
 	perror( "snet_read" );
-	exit( 1 );
+	return( -1 );
     }
-    if ( chksum ) SHA1_Update( &sha_ctx, ( char * )&ae_finfo, (size_t)as_cc );
+    if ( chksum ) SHA1_Update( &sha_ctx, ( char * )&ae_ents, (size_t)rc );
     if ( dodots ) { putc( '.', stdout ); fflush( stdout ); }
 
-    size -= as_cc;
+    size -= rc;
 
-    /* read rsrc fork header entry */
+    /* read finder info */
     tv = timeout;
-    if (( as_cc = snet_read( sn, ( char * )&ae_rfork,
-		sizeof( struct as_entry ), &tv )) <= 0 ) {
+    if (( rc = snet_read( sn, finfo, sizeof( finfo ), &tv ))
+		!= sizeof( finfo )) {
 	perror( "snet_read" );
-	exit( 1 );
+	return( -1 );
     }
-    if ( chksum ) SHA1_Update( &sha_ctx, ( char * )&ae_rfork, (size_t)as_cc );
+    if ( chksum ) SHA1_Update( &sha_ctx, finfo, (size_t)rc );
     if ( dodots ) { putc( '.', stdout ); fflush( stdout ); }
+    size -= rc;
 
-    size -= as_cc;
-
-    /* read data fork header entry */
-    tv = timeout;
-    if (( as_cc = snet_read( sn, ( char * )&ae_dfork,
-		sizeof( struct as_entry ), &tv )) <= 0 ) {
-	perror( "snet_read" );
-	exit( 1 );
-    }
-    if ( chksum ) SHA1_Update( &sha_ctx, ( char * )&ae_dfork, (size_t)as_cc );
-    if ( dodots ) { putc( '.', stdout ); fflush( stdout ); }
-
-    size -= as_cc;
-
-    if (( ofd = open( temppath, O_CREAT | O_EXCL | O_WRONLY, 0666 )) < 0 ) {
-	perror( temppath );
-	exit( 1 );
+    /* name temp file */
+    if ( snprintf( temppath, MAXPATHLEN, "%s.radmind.%i", path,
+	    getpid()) > MAXPATHLEN ) {
+	fprintf( stderr, "%s.radmind.%i: too long", path, ( int )getpid());
+	return( -1 );
     }
 
-    tv = timeout;
-    if (( as_cc = snet_read( sn, finfo, sizeof( finfo ), &tv )) < 0 ) {
-	perror( "snet_read" );
-	exit( 1 );
+    /* make rsrc fork name */
+    if ( snprintf( rsrc_path, MAXPATHLEN, "%s%s", temppath,
+		_PATH_RSRCFORKSPEC ) > MAXPATHLEN ) {
+	fprintf( stderr, "%s%s: path too long\n", temppath,
+		_PATH_RSRCFORKSPEC );
+	return ( -1 );
     }
-    if ( chksum ) SHA1_Update( &sha_ctx, finfo, (size_t)as_cc );
-    if ( dodots ) { putc( '.', stdout ); fflush( stdout ); }
-    size -= as_cc;
 
-    if ( snprintf( rsrc_path, MAXPATHLEN, "%s%s", temppath, rsrc_suffix ) >
-		MAXPATHLEN ) {
-	fprintf( stderr, "%s%s: path too long\n", temppath, rsrc_suffix );
-	exit( 1 );
+    /* data fork must exist to write to rsrc fork */        
+    if (( dfd = open( temppath, O_CREAT | O_EXCL | O_WRONLY, 0666 )) < 0 ) {
+	goto error1;
     }
-        
-    if (( rsrcfd = open( rsrc_path, O_WRONLY, 0 )) < 0 ) {
-        perror( rsrc_path );
-        exit( 1 );
+
+    if (( rfd = open( rsrc_path, O_WRONLY, 0 )) < 0 ) {
+        goto error2;
     };  
 
-    rsize = ae_rfork.ae_length;
-   
-    while ( rsize > 0 ) { 
+    for ( rc = 0, rsize = ae_ents[ AS_RFE ].ae_length; rsize > 0;
+		rsize -= rc ) {
 	tv = timeout;
-	if (( as_cc = snet_read( sn, as_buf, ( int )MIN( sizeof( as_buf ),
-		rsize ), &tv )) < 0 ) {
-	    perror( "snet_read" );
-	    exit( 1 );
-	}
-
-	if (( write( rsrcfd, as_buf, ( unsigned int )as_cc )) != as_cc ) {
-	    perror( "rsrcfd write" );
-	    exit( 1 );
-	}
-	if ( chksum ) SHA1_Update( &sha_ctx, as_buf, (size_t)as_cc );
-	if ( dodots ) { putc( '.', stdout ); fflush( stdout ); }
-	rsize -= as_cc;
-    }
-
-    if ( rsize != 0 ) {
-	fprintf( stderr, "Didn't write correct number of bytes to rsrc fork" );
-	exit( 1 );
-    }
-    
-    if ( close( rsrcfd ) < 0 ) {
-	perror( "close rsrcfd" );
-	exit( 1 );
-    }
-
-    size -= ae_rfork.ae_length;
-
-    /* write data fork to file */
-    while ( size > 0 ) {
-    	tv = timeout;
-    	if (( as_cc = snet_read( sn, as_buf, (int)MIN( sizeof( as_buf ), size),
+	if (( rc = snet_read( sn, buf, ( int )MIN( sizeof( buf ), rsize ),
 		&tv )) <= 0 ) {
 	    perror( "snet_read" );
-	    exit( 1 );
+	    goto error2;
 	}
 
-	if ( write( ofd, as_buf, ( unsigned int )as_cc ) != as_cc ) {
-	    perror( "ofd write" );
-	    exit( 1 );
+	if (( write( rfd, buf, ( unsigned int )rc )) != rc ) {
+	    perror( "rfd write" );
+	    goto error2;
+	}
+	if ( chksum ) SHA1_Update( &sha_ctx, buf, (size_t)rc );
+	if ( dodots ) { putc( '.', stdout ); fflush( stdout ); }
+    }
+
+    size -= ae_ents[ AS_RFE ].ae_length;
+    
+    if ( close( rfd ) < 0 ) {
+	perror( "close rfd" );
+	exit( 1 );
+    }
+
+    /* write data fork to file */
+    for ( rc = 0; size > 0; size -= rc ) {
+    	tv = timeout;
+    	if (( rc = snet_read( sn, buf, (int)MIN( sizeof( buf ), size ),
+		&tv )) <= 0 ) {
+	    perror( "snet_read" );
+	    goto error2;
 	}
 
-	if ( chksum ) SHA1_Update( &sha_ctx, as_buf, (size_t)as_cc );
+	if ( write( dfd, buf, ( unsigned int )rc ) != rc ) {
+	    perror( "dfd write" );
+	    goto error2;
+	}
+
+	if ( chksum ) SHA1_Update( &sha_ctx, buf, (size_t)rc );
 	if ( dodots ) { putc( '.', stdout ); fflush( stdout); }
-	size -= as_cc;
+    }
+
+    if ( close( dfd ) < 0 ) {
+	perror( "close dfd" );
+	exit( 1 );
     }
     if ( verbose ) printf( "\n" );
 
-    if ( close( ofd ) < 0 ) {
-	perror( "close ofd" );
-	exit( 1 );
-    }
-
-    if (( rc = setattrlist( temppath, &alist, finfo, 32, FSOPT_NOFOLLOW ))) {
-	perror( "setattrlist" );
-	exit( 1 );
+    /* set finder info for newly decoded applefile */
+    if ( setattrlist( temppath, &alist, finfo, sizeof( finfo ),
+		FSOPT_NOFOLLOW ) != 0 ) {
+	goto error3;
     }
 
     tv = timeout;
-    if ( ( line = snet_getline( sn, &tv ) ) == NULL ) {
+    if (( line = snet_getline( sn, &tv )) == NULL ) {
         fprintf( stderr, "snet_getline" );
-        exit( 1 );
+        return( -1 );
     }
     if ( strcmp( line, "." ) != 0 ) {
         fprintf( stderr, "%s", line );
-        exit( 1 );
+        return( -1 );
     }
     if ( verbose ) printf( "<<< .\n" );
 
-    /* Chksum file */
     if ( chksum ) {
         SHA1_Final( md, &sha_ctx );
         base64_e( md, sizeof( md ), mde );
         if ( strcmp( chksumval, mde ) != 0 ) {
             fprintf( stderr, "checksum failed: %s\n", path );
-            exit( 1 );
+            return( -1 );
         }
     }
 
     return( 0 );
+
+error1:
+    if ( close( dfd ) < 0 ) {
+	perror( temppath );
+	exit( 1 );
+    }
+
+    return( -1 );
+error2:
+    if ( close ( dfd ) < 0 ) {
+	perror( temppath );
+	exit( 1 );
+    }
+
+    if ( close( rfd ) < 0 ) {
+	perror( rsrc_path );
+	exit( 1 );
+    }
+
+    return( -1 );
+error3:
+    fprintf( stderr, "Couldn't set finder info for %s.\n", temppath );
+    return( -1 );
 }
-
-/*
- * applesingle format:
- *  header:
- *	-magic number (4 bytes)
- *	-version number (4 bytes)
- *	-filler (16 bytes)
- *	-number of entries (2 bytes)
- *	-x number of entries, with this format:
- *	    id (4 bytes)
- *	    offset (4 bytes)
- *	    length (4 bytes)
- *  data:
- *	-finder info
- *	-rsrc fork
- *	-data fork
- */
-
 #else !__APPLE__
-
 #include <sys/time.h>
-
 #include <snet.h>
-
 #include "applefile.h"
-
     int
 retr_applefile( SNET *sn, char *pathdesc, char *path, char *chksumval,
     char *temppath, int linenum )
 {
     return( -1 );
 }
-
 #endif __APPLE__
--- applefile.h
+++ applefile.h
@@ -1,11 +1,32 @@
+/*
+ * applesingle format:
+ *  header:
+ *      -magic number (4 bytes)
+ *      -version number (4 bytes)
+ *      -filler (16 bytes)
+ *      -number of entries (2 bytes)
+ *      -x number of entries, with this format:
+ *          id (4 bytes)
+ *          offset (4 bytes)
+ *          length (4 bytes)
+ *  data:
+ *      -finder info
+ *      -rsrc fork
+ *      -data fork
+ */
+
 #define AS_HEADERLEN	26
 #define AS_MAGIC	0x00051600
 #define AS_VERSION	0x00020000
-#define NUM_ENTRIES	3
+#define AS_NENTRIES	3
 
 #define ASEID_DFORK	1
 #define ASEID_RFORK	2
 #define ASEID_FINFO	9
+
+#define AS_FIE		0	/* for array of ae_entry structs */
+#define AS_RFE		1
+#define AS_DFE		2	
 
 struct as_entry {
     unsigned long	ae_id;
@@ -20,5 +41,6 @@ struct as_header {
     unsigned short	ah_num_entries;
 };
 
-int retr_applefile( SNET *sn, char *pathdesc, char *path, char *chksumval,
-    char *temppath, int linenum );
+int retr_applefile( SNET *sn, char *pathdesc, char *path, 
+	char *chksumval, char *temppath, int linenum );
+int stor_applefile( int dfd, SNET *sn, char *filename );
