8f719d4e9ca9d2f23a09fe57797a081723e14b47
editor <editor>
2003-01-10 18:03:13 +0000

    Added copyright info

--- tls.h
+++ tls.h
@@ -1,2 +1,7 @@
+/*
+ * Copyright (c) 2003 Regents of The University of Michigan.
+ * All Rights Reserved.  See COPYRIGHT.
+ */
+
 int tls_client_setup( int use_randfile, int authlevel, char *ca, char *cert, char *privatekey );
 int tls_client_start( SNET *sn, char *host, int authlevel );
--- wildcard.c
+++ wildcard.c
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2003 Regents of The University of Michigan.
+ * All Rights Reserved.  See COPYRIGHT.
+ */
+
 #include <stdlib.h>
 #include <ctype.h>
 
