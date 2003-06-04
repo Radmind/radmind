0e0a509c6d0bff826a5e2de101082cd61d56f5f0
wes <wes>
2003-06-04 20:47:51 +0000

    added support for non-largefile systems

--- largefile.h
+++ largefile.h
@@ -3,5 +3,10 @@
  * All Rights Reserved.  See COPYRIGHT.
  */
 
+#if SIZE_OFF_T == 8
 #define strtoofft(x,y,z)	(strtoll((x),(y),(z)))
 #define PRIofft			"ll"
+#else	/* a bit of an assumption, here */
+#define strtoofft(x,y,z)	(strtol((x),(y),(z)))
+#define PRIofft			"l"
+#endif
