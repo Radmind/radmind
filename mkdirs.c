30af76a987a9145f5e41cb0a5d695287412ee257
mcneal <mcneal>
2003-10-03 15:35:20 +0000

    Removed erroneous else.

--- mkdirs.c
+++ mkdirs.c
@@ -24,9 +24,8 @@ mkdirs( char *path )
 	if ( mkdir( path, 0777 ) < 0 ) {
 	    if ( errno != EEXIST ) {
 		return( -1 );
-	    } else {
-		errno = 0;
 	    }
+	    errno = 0;
 	}
 	*p++ = '/';
     }
