2265f6b45e7cf83f81bade79d0fcd78be9c85c67
mcneal <mcneal>
2004-03-04 22:06:15 +0000

    Transcript root code.

---
+++ root.c
@@ -0,0 +1,92 @@
+#include <sys/param.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#include "root.h"
+
+    int
+get_root( char *path, char *file_root, char *tran_root, char *tran_name )
+{
+    char		real_path[ PATH_MAX ];
+    char		test_path[ MAXPATHLEN ];
+    char                radmind_real_path[ PATH_MAX ];
+    char		*p;
+
+    if ( realpath( _RADMIND_PATH, radmind_real_path ) == NULL ) {
+        perror( radmind_real_path );
+        return( -1 );
+    }
+    if ( realpath( path, real_path ) == NULL ) {
+	perror( real_path );
+        return( -1 );
+    }
+
+    /* Get transcript name */
+    if (( p = strrchr( real_path, '/' )) == real_path ) {
+	fprintf( stderr, "%s: invalid transcript location\n", real_path );
+	return( -1 );
+    }
+    *p++ = '\0';
+    if ( snprintf( tran_name, MAXPATHLEN, "%s", p ) > MAXPATHLEN - 1 ) {
+        fprintf( stderr, "%s: path too long\n", p );
+        return( -1 );
+    }
+
+    if ( snprintf( test_path, MAXPATHLEN, "%s/tmp/transcript",
+            radmind_real_path ) > MAXPATHLEN - 1 ) {
+        fprintf( stderr, "%s/tmp/transcirpt: path too long\n",
+	    radmind_real_path );
+        return( -1 );
+    }
+
+    if ( strstr( real_path, test_path ) != NULL ) {
+        if ( snprintf( file_root, MAXPATHLEN, "%s/tmp/file%s",
+		radmind_real_path, &real_path[ strlen( test_path ) ])
+		> MAXPATHLEN - 1 ) {
+            fprintf( stderr, "%s/tmp/file%s: path too long\n",
+		radmind_real_path, &real_path[ strlen( test_path )] );
+            return( -1 );
+        }
+        if ( snprintf( tran_root, MAXPATHLEN, "%s/tmp/transcript%s",
+		radmind_real_path, &real_path[ strlen( test_path ) ])
+		> MAXPATHLEN - 1 ) {
+            fprintf( stderr, "%s/tmp/transcript%s: path too long\n",
+		radmind_real_path, &real_path[ strlen( test_path )] );
+            return( -1 );
+        }
+    } else {
+        if ( snprintf( test_path, MAXPATHLEN, "%s/transcript",
+		radmind_real_path ) > MAXPATHLEN - 1 ) {
+            fprintf( stderr, "%s/transcript: path too long\n",
+		radmind_real_path );
+            return( -1 );
+        }
+
+        if ( strstr( real_path, test_path ) != NULL ) {
+            if ( snprintf( file_root, MAXPATHLEN, "%s/file%s",
+                    radmind_real_path, &real_path[ strlen( test_path ) ])
+		    > MAXPATHLEN - 1 ) {
+                fprintf( stderr, "%s/file%s: path too long\n",
+                    radmind_real_path, &real_path[ strlen( test_path ) ]);
+                return( -1 );
+            }
+	    if ( snprintf( tran_root, MAXPATHLEN, "%s/transcript%s",
+		    radmind_real_path, &real_path[ strlen( test_path ) ])
+		    > MAXPATHLEN - 1 ) {
+		fprintf( stderr, "%s/transcript%s: path too long\n",
+		    radmind_real_path, &real_path[ strlen( test_path )] );
+		return( -1 );
+	    }
+        } else {
+            sprintf( file_root, "%s/../file", real_path );
+            sprintf( tran_root, "%s", real_path );
+        }
+    }
+    printf( "file_root: %s\n", file_root );
+    printf( "tran_root: %s\n", tran_root );
+    printf( "tran_name: %s\n", tran_name );
+
+    return( 0 );
+}
---
+++ root.h
@@ -0,0 +1,1 @@
+int get_root( char *path, char *file_root, char *tran_root, char *tran_name );
