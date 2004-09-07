e5cd24915b31e57750e5b05a94f12412c431a164
mcneal <mcneal>
2004-09-07 18:32:40 +0000

    Check for SIZEOF_OFF_T

--- largefile.h
+++ largefile.h
@@ -3,6 +3,10 @@
  * All Rights Reserved.  See COPYRIGHT.
  */
 
+#ifndef SIZEOF_OFF_T
+#error "sizeof off_t unknown."
+#endif
+
 #if SIZEOF_OFF_T == 8
 #define strtoofft(x,y,z)	(strtoll((x),(y),(z)))
 #define PRIofft			"ll"
