1fd27683ea2c09d3ad4ec8cd55a6c6d1e8eb83a6
admorten <admorten>
2005-03-02 02:06:19 +0000

    Removed unused trailing slash handler.

--- pathcmp.c
+++ pathcmp.c
@@ -19,36 +19,6 @@ pathcmp( const unsigned char *p1, const unsigned char *p2 )
 
     do {
 	if (( rc = ( *p1 - *p2 )) != 0 ) {
-#ifdef notdef
-	    /*
-	     * This (untested) code might make a trailing slash not matter
-	     * during path comparison.  Of course, that means files with
-	     * trailing slashes become legal....
-	     *
-	     * A better way to handle the issue of TAB'd filename completion
-	     * is probably to make "lcheck" give a warning.
-	     */
-	    if ( *p1 == '/' ) {
-		if ( *p2 == '\0' ) {
-		    if ( *( p1 + 1 ) == '\0' ) {
-			return( 0 );
-		    }
-		} else {
-		    return( -1 );
-		}
-	    } else if ( *p2 == '/' ) {
-		if ( *p1 == '\0' ) {
-		    if ( *( p2 + 1 ) == '\0' ) {
-			return( 0 );
-		    }
-		} else {
-		    return( 1 );
-		}
-	    } else {
-		return( rc );
-	    }
-#endif /* notdef */
-
 	    if (( *p2 != '\0' ) && ( *p1 == '/' )) {
 		return( -1 );
 	    } else if (( *p1 != '\0' ) && ( *p2 == '/' )) {
