0dde70813ba775c8462c3afec3538c53b0450fa5
mcneal <mcneal>
2005-06-14 14:13:54 +0000

    Production version of lsort

--- lsort.c
+++ lsort.c
@@ -1,218 +1,220 @@
 #include <sys/param.h>
+
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int exitrc;
+#include <unistd.h>
 
-/************* start stuff from radmind source code ************************/
-/* Just like strcmp(), but pays attention to the meaning of '/'.  */
-    int 
-pathcmp( const unsigned char *p1, const unsigned char *p2 )
-{
-    int		rc;
+#include "argcargv.h"
+#include "code.h"
+#include "pathcmp.h"
 
-    do {
-	if (( rc = ( *p1 - *p2 )) != 0 ) {
-	    if (( *p2 != '\0' ) && ( *p1 == '/' )) {
-		return( -1 );
-	    } else if (( *p1 != '\0' ) && ( *p2 == '/' )) {
-		return( 1 );
-	    } else {
-		return( rc );
-	    }
-	}
-	p2++;
-    } while ( *p1++ != '\0' );
-
-    return( 0 );
-}
-
-    char *
-decode( char *line ) 
-{
-    /* static */
-    static char     buf[ MAXPATHLEN ];
-    char	    *temp;
-
-    if ( strlen( line ) > ( 2 * MAXPATHLEN )) {
-	return( NULL );
-    }
-
-    temp = buf;
-
-    for ( ; *line != '\0'; line++, temp++ ) {
-	switch( *line ) {
-	case '\\':
-	    line++;
-	    switch( *line ) {
-	    case 'n':
-		*temp = '\n';
-		break;
-	    case 't':
-		*temp = '\t';
-		break;
-	    case 'b':
-		*temp = ' ';
-		break;
-	    case 'r':
-		*temp = '\r';
-		break;
-	    case '\\':
-		*temp = '\\';
-		break;
-	    default:
-		break;
-	    }
-	    break;
-	default:
-	    *temp = *line;
-	    break;
-	}
-    }
-
-    *temp = '\0';
-    return( buf );
-}
-
-/************* end stuff from radmind source code ************************/
+int	linecount;
+FILE	*outtran;
 
 struct save_line {
 	struct save_line *next;
 	char *key;
 	char *data;
 } *lines;
-int linecount;
+
+void save_it( char *buffer, char *pathname );
+static int lsort_cmp( const void *a1, const void *b1 );
+void sort_them( void );
+void print_them( void );
+void process( char * arg );
+
+int		case_sensitive = 1;
 
     void
-save_it( char *buffer, char *pathname)
+save_it( char *buffer, char *pathname )
 {
-	struct save_line *sp;
-	sp = malloc(sizeof *sp + strlen(buffer) + strlen(pathname) + 4);
-	sp->key = (char*) (sp+1);
-	strcpy(sp->key, pathname);
-	sp->data = (sp->key + strlen(sp->key) + 1);
-	strcpy(sp->data, buffer);
-	sp->next = lines;
-	lines = sp;
-	++linecount;
+    struct save_line 	*sp;
+
+    sp = malloc(sizeof *sp + strlen(buffer) + strlen(pathname) + 4);
+    sp->key = (char*) (sp+1);
+    strcpy(sp->key, pathname);
+    sp->data = (sp->key + strlen(sp->key) + 1);
+    strcpy(sp->data, buffer);
+    sp->next = lines;
+    lines = sp;
+    linecount++;
 }
 
-    int
-lsort_cmp( void *a1, void *b1 )
+    static int
+lsort_cmp( const void *a1, const void *b1 )
 {
-	const struct save_line **a, **b;
+    const struct	save_line **a, **b;
 
-	a = a1;
-	b = b1;
-	return pathcmp((*a)->key, (*b)->key);
+    a = (const struct save_line**)a1;
+    b = (const struct save_line**)b1;
+
+    return( pathcmp_case((*a)->key, (*b)->key, case_sensitive ));
 }
 
     void
 sort_them()
 {
-	struct save_line **x, *sp, **y;
-	x = (struct save_line**) malloc(sizeof *x * linecount);
-	y = x;
-	for (sp = lines; sp; sp = sp->next)
-		*y++ = sp;
-	qsort(x, linecount, sizeof *x, lsort_cmp);
-	sp = 0;
-	while (y-- != x)
-	{
-		(*y)->next = sp;
-		sp = (*y);
-	}
-	lines = sp;
+    struct save_line **x, *sp, **y;
+
+    x = (struct save_line**) malloc( sizeof *x * linecount );
+    y = x;
+
+    for ( sp = lines; sp; sp = sp->next ) {
+	*y++ = sp;
+    }
+    
+    qsort( x, linecount, sizeof *x, lsort_cmp );
+
+    sp = 0;
+    while ( y-- != x ) {
+	(*y)->next = sp;
+	sp = (*y);
+    }
+
+    lines = sp;
 }
 
     void
 print_them()
 {
-	struct save_line *sp;
-	for (sp = lines; sp; sp = sp->next)
-	{
-		fputs(sp->data, stdout);
-		if (ferror(stdout))
-		{
-			perror("writing stdout");
-			exitrc |= 4;
-			break;
-		}
+    struct save_line *sp;
+    for ( sp = lines; sp; sp = sp->next ) {
+	fputs(sp->data, outtran );
+	if ( ferror( outtran )) {
+	    perror( "writing stdout" );
+	    exit( 1 );
 	}
+    }
 }
 
     void
 process( char * arg )
 {
-	FILE *fd;
-	char buffer[4096];
-	char control[4096];
-	char pathname[4096];
-	char *fn;
-	char *cp;
-	int lineno;
+    FILE	*fd;
+    ACAV	*acav;
+    char	buffer[4096];
+    char	*fn;
+    int		lineno, argc;
+    char	**argv;
+    char	*line = NULL;
 
-	if (strcmp(arg, "-"))
-	{
-		fn = arg;
-		fd = fopen(arg, "r");
-	}
-	else
-	{
-		fn = "(stdin)";
-		fd = stdin;
-	}
-	if (!fd)
-	{
-		perror(arg);
-		exitrc |= 1;
-		return;
-	}
+    if ( strcmp( arg, "-" )) {
+	fn = arg;
+	fd = fopen( arg, "r" );
+    } else {
+	fn = "(stdin)";
+	fd = stdin;
+    }
+    if ( !fd ) {
+	    perror(arg);
+	    exit( 1 );
+    }
 
-	lineno = 0;
-	while (fgets(buffer, sizeof buffer, fd))
-	{
-		++lineno;
-		cp = buffer;
-		if (*cp == '-' && cp[1] == ' ') cp += 2;
-		if (sscanf(cp, "%s %s ", control, pathname) != 2)
-		{
-fprintf(stderr,"%s(%d): discarding because not enough fields\n", fn, lineno);
-			exitrc |= 2;
-			continue;
-		}
-		save_it(buffer,decode(pathname));
+    acav = acav_alloc( );
+
+    lineno = 0;
+    while ( fgets( buffer, sizeof buffer, fd )) {
+	lineno++;
+
+	if (( line = strdup( buffer )) == NULL ) {
+	    perror( "strdup" );
+	    exit( 1 );
 	}
 
-	if (fd == stdin)
-		clearerr(fd);
-	else
-		fclose(fd);
+	argc = acav_parse( acav, buffer, &argv );
+
+	/* Skip blank lines */
+	if ( argc == 0 ) {
+	    continue;
+	}
+
+	/* XXX - Drop comments - how would you sort them? */
+	if ( *argv[ 0 ] == '#' ) {
+	    continue;
+	}
+
+	/* Get argument offset */
+	if (( *argv[ 0 ] ==  '+' ) || ( *argv[ 0 ] == '-' )) {
+	    argv++;
+	    argc--;
+	}
+
+	if ( argc < 2 ) {
+	    fprintf( stderr,
+		"%s: line %d: not enough fields\n",
+		fn, lineno);
+	    exit( 1 );
+	}
+	save_it( line, decode( argv[ 1 ] ));
+    }
+
+    if ( fd == stdin ) {
+	clearerr( fd );
+    } else {
+	fclose( fd );
+    }
+
+    free( line );
+    acav_free( acav );
+
 }
 
     int
-main( int argc, char **argv)
+main( int argc, char **argv )
 {
-	char *argp;
-	int didit;
+    char	c;
+    int		i, err = 0;
+    extern char	*version;
 
-	didit = 0;
+    outtran = stdout;
 
-	while (--argc > 0) if (*(argp = *++argv) == '-')
-	if (argp[1]) while (*++argp) switch(*argp)
-	{
-	case '-':
+    while (( c = getopt( argc, argv, "Io:V" )) != EOF ) {
+	switch( c ) {
+	    case 'I':
+		case_sensitive = 0;
 		break;
-	default:
-		fprintf (stderr,
-			"Usage: lsort [files...]\n");
-		exit(1);
+
+	    case 'o':
+		if (( outtran = fopen( optarg, "w" )) == NULL ) {
+		    perror( optarg );
+		    exit( 1 );
+		}
+		break;
+
+	    case 'V':
+		printf( "%s\n", version );
+		exit( 0 );
+
+	    default:
+		err++;
+		break;
 	}
-	else ++didit, process("-");
-	else ++didit, process(argp);
-	if (!didit) process("-");
-	sort_them();
-	print_them();
-	exit(exitrc);
+    }
+
+    if ( err ) {
+	fprintf( stderr, "Usage: %s [ -IV ] ", argv[ 0 ] );
+	fprintf( stderr, "[ -o file ] [ files... ]\n" );
+	exit( 1 );
+    }
+
+    if ( argc - optind == 0 ) {
+	/* Only stdin */
+	process( "-" );
+    } else {
+	/* Process all args */
+	for ( i = optind; i < argc; i++ ) {
+	    if ( strcmp( argv[ i ], "-" ) == 0 ) {
+		process( "-" );
+	    } else {
+		process( argv[ i ] );
+	    }
+	}
+    }
+
+    sort_them();
+    print_them();
+
+    exit( 0 );
 }
