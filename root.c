98f0c17718e8f2be7d98c451f9a197e61972f285
fitterhappier <fitterhappier>
2008-01-15 21:01:10 +0000

    Use >= MAXPATHLEN instead of > MAXPATHEN - 1. Bounds checking in edge case.

--- root.c
+++ root.c
@@ -38,7 +38,7 @@ get_root( char *radmind_path, char *path, char *file_root, char *tran_root, char
     strcpy( tran_name, p );
 
     if ( snprintf( test_path, MAXPATHLEN, "%s/tmp/transcript",
-            radmind_real_path ) > MAXPATHLEN - 1 ) {
+            radmind_real_path ) >= MAXPATHLEN ) {
         fprintf( stderr, "%s/tmp/transcirpt: path too long\n",
 	    radmind_real_path );
         return( -1 );
@@ -47,21 +47,21 @@ get_root( char *radmind_path, char *path, char *file_root, char *tran_root, char
     if ( strstr( real_path, test_path ) != NULL ) {
         if ( snprintf( file_root, MAXPATHLEN, "%s/tmp/file%s",
 		radmind_real_path, &real_path[ strlen( test_path ) ])
-		> MAXPATHLEN - 1 ) {
+		>= MAXPATHLEN ) {
             fprintf( stderr, "%s/tmp/file%s: path too long\n",
 		radmind_real_path, &real_path[ strlen( test_path )] );
             return( -1 );
         }
         if ( snprintf( tran_root, MAXPATHLEN, "%s/tmp/transcript%s",
 		radmind_real_path, &real_path[ strlen( test_path ) ])
-		> MAXPATHLEN - 1 ) {
+		>= MAXPATHLEN ) {
             fprintf( stderr, "%s/tmp/transcript%s: path too long\n",
 		radmind_real_path, &real_path[ strlen( test_path )] );
             return( -1 );
         }
     } else {
         if ( snprintf( test_path, MAXPATHLEN, "%s/transcript",
-		radmind_real_path ) > MAXPATHLEN - 1 ) {
+		radmind_real_path ) >= MAXPATHLEN ) {
             fprintf( stderr, "%s/transcript: path too long\n",
 		radmind_real_path );
             return( -1 );
@@ -70,21 +70,21 @@ get_root( char *radmind_path, char *path, char *file_root, char *tran_root, char
         if ( strstr( real_path, test_path ) != NULL ) {
             if ( snprintf( file_root, MAXPATHLEN, "%s/file%s",
                     radmind_real_path, &real_path[ strlen( test_path ) ])
-		    > MAXPATHLEN - 1 ) {
+		    >= MAXPATHLEN ) {
                 fprintf( stderr, "%s/file%s: path too long\n",
                     radmind_real_path, &real_path[ strlen( test_path ) ]);
                 return( -1 );
             }
 	    if ( snprintf( tran_root, MAXPATHLEN, "%s/transcript%s",
 		    radmind_real_path, &real_path[ strlen( test_path ) ])
-		    > MAXPATHLEN - 1 ) {
+		    >= MAXPATHLEN ) {
 		fprintf( stderr, "%s/transcript%s: path too long\n",
 		    radmind_real_path, &real_path[ strlen( test_path )] );
 		return( -1 );
 	    }
         } else {
-            sprintf( file_root, "%s/../file", real_path );
-            sprintf( tran_root, "%s", real_path );
+            snprintf( file_root, MAXPATHLEN, "%s/../file", real_path );
+            snprintf( tran_root, MAXPATHLEN, "%s", real_path );
         }
     }
 
