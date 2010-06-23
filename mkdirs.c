ce09d4c6f94f552b971c69affbae4bd62e8d385c
Andrew Mortensen <admorten@umich.edu>
2010-06-23 16:51:49 -0400

    Create longest possible path first in mkdirs routine.
    Apply patch from Wes.

--- mkdirs.c
+++ mkdirs.c
@@ -12,31 +12,51 @@
 
 #include "mkdirs.h"
 
+/*
+ * The right most element of the path is assumed to be a file.
+ */
     int 
 mkdirs( char *path ) 
 {
-    char 	*p;
+    char 	*p, *q = NULL;
     int		saved_errno;
-    struct stat	st;
 
-    for ( p = path; *p == '/'; p++ )
-	;
-    for ( p = strchr( p, '/' ); p != NULL; p = strchr( p, '/' )) {
-	*p = '\0';
-	if ( mkdir( path, 0777 ) < 0 ) {
-	    saved_errno = errno;
-	    if ( stat( path, &st ) != 0 ) {
-		errno = saved_errno;
-		return( -1 );
-	    }
-	    if ( !S_ISDIR( st.st_mode )) {
-		errno = EEXIST;
-		return( -1 );
-	    }
-	    errno = 0;
+    saved_errno = errno;
+
+    /* try making longest path first, working backward */
+    for (;;) {
+	if (( p = strrchr( path, '/' )) == NULL ) {
+	    errno = EINVAL;
+	    return( -1 );
 	}
-	*p++ = '/';
+	*p = '\0';
+	if ( q != NULL ) {
+	    *q = '/';
+	}
+
+	if ( mkdir( path, 0777 ) == 0 ) {
+	    break;
+	}
+	if ( errno != ENOENT ) {
+	    return( -1 );
+	}
+	q = p;
     }
+
+    *p = '/';
+
+    if ( q != NULL ) {
+	p++;
+	for ( p = strchr( p, '/' ); p != NULL; p = strchr( p, '/' )) {
+	    *p = '\0';
+	    if ( mkdir( path, 0777 ) < 0 ) {
+		return( -1 );
+	    }
+	    *p++ = '/';
+	}
+    }
+
+    errno = saved_errno;
 
     return( 0 );
 }
