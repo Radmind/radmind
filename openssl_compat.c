c4748feef8fdc88e2a9deb1f14bb2ca15a5fc2b8
michael.graziano@premierheart.com <michael.graziano@premierheart.com>
2018-10-15 15:32:09 -0400

    OpenSSL 1.1 compatibility & Pre-1.1 shims.
    - Handle OpenSSL 1.1 opaque structures for EVP digest functions
    - Add openssl_compat.c shims file to support pre-1.1 OpenSSL installs
      (ref. https://wiki.openssl.org/index.php/OpenSSL_1.1.0_Changes)

---
+++ openssl_compat.c
@@ -0,0 +1,24 @@
+/*
+ * OpenSSL Compatibility Shims for pre-1.1.0
+ *
+*/
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#ifndef RADMIND_OPENSSL_SHIMS
+#define RADMIND_OPENSSL_SHIMS
+
+#include <string.h>
+#include <openssl/engine.h>
+
+EVP_MD_CTX *EVP_MD_CTX_new(void)
+{
+    return OPENSSL_zalloc(sizeof(EVP_MD_CTX));
+}
+
+void EVP_MD_CTX_free(EVP_MD_CTX *ctx)
+{
+   EVP_MD_CTX_cleanup(ctx);
+   OPENSSL_free(ctx);
+}
+
+#endif // OPENSSL_RADMIND_SHIMS
+#endif // OLD OPENSSL <1.1.0
