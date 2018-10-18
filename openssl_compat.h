e6c3ecd8bb2d25485717fad5c061711f2a80d0fa
michael.graziano@premierheart.com <michael.graziano@premierheart.com>
2018-10-18 18:06:01 -0400

    OpenSSL Compat shim fixups - Version check.
    - Can't check OpenSSL version until we include OpenSSL headers
    - Don't need extra shim safety since we build the object files separately

--- openssl_compat.h
+++ openssl_compat.h
@@ -2,15 +2,12 @@
  * OpenSSL Compatibility Shims for pre-1.1.0
  *
 */
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
-#ifndef RADMIND_OPENSSL_SHIMS
-#define RADMIND_OPENSSL_SHIMS
 
 #include <string.h>
 #include <openssl/engine.h>
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
 
 EVP_MD_CTX *EVP_MD_CTX_new(void);
 void EVP_MD_CTX_free(EVP_MD_CTX *ctx);
 
-#endif // OPENSSL_RADMIND_SHIMS
 #endif // OLD OPENSSL <1.1.0
